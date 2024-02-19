3a69a519e226c9e68bfdf5bf27b49c930e6650bb
tookuyam <tookuyam@student.42.tokyo.jp>
2024-02-19 18:17:45 +0900

    fix: install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+TMP_LIBFT_DIR=tmpLibft
+LIBFT_REPO=https://github.com/PalmNeko/Libt
+
 cd $(dirname "$0") || ( echo 'error change dir' && exit 1 )
-cp -r ../Libft/submit/ ./submit/Libft
+
+git clone ${LIBFT_REPO} ./${TMP_LIBFT_DIR}
+
+cp -r ./${TMP_LIBFT_DIR}/submit ./submit/Libft
+
+rm -rf ./${TMP_LIBFT_DIR}

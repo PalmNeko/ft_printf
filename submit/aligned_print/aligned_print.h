af82da0ec54e4bf437f289f9bd4b603143b60055
tookuyam <tookuyam@student.42.tokyo.jp>
2024-02-20 13:19:45 +0900

    fix: merge aligned_print

--- submit/aligned_print/aligned_print.h
+++ submit/aligned_print/aligned_print.h
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 15:56:23 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/02/11 14:37:20 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/02/20 13:15:56 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,7 @@
 # include <stddef.h>
 # include "conversion_specification.h"
 
-int	left_aligned_print(int fd, const char *str, size_t min_len);
-int	right_aligned_print(int fd, const char *str, size_t min_len, int pad_zero);
 int	t_conv_aligned_print(int fd, t_cs *cs, const char *str);
-int	left_aligned_print_char(int fd, char c, size_t min_len);
-int	right_aligned_print_char(int fd, char c, size_t min_len, int pad_zero);
 int	t_conv_aligned_print_char(int fd, t_cs *cs, char c);
 
 #endif
--- submit/aligned_print/t_conv_aligned_print.c
+++ submit/aligned_print/t_conv_aligned_print.c
@@ -6,13 +6,21 @@
 /*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:08:00 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/02/12 15:16:29 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/02/20 13:16:00 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+#include <limits.h>
+#include <stdio.h>
+#include "libft.h"
+#include "string_util.h"
 #include "conversion_specification.h"
 #include "conversion_specification_utils.h"
 #include "aligned_print.h"
+
+int	left_aligned_print(int fd, const char *str, size_t min_len);
+int	right_aligned_print(int fd, const char *str, size_t min_len, int pad_zero);
 
 int	t_conv_aligned_print(int fd, t_cs *cs, const char *str)
 {
@@ -28,4 +36,53 @@ int	t_conv_aligned_print(int fd, t_cs *cs, const char *str)
 	else
 		print_len = right_aligned_print(fd, str, min_len, cs->flag_zero);
 	return (print_len);
+}
+
+int	left_aligned_print(int fd, const char *str, size_t min_len)
+{
+	size_t	len;
+	int		now_print_len;
+
+	len = ft_strlen(str);
+	if (len > INT_MAX || min_len > INT_MAX)
+		return (-1);
+	if (write(fd, str, len) < 0)
+		return (-1);
+	if (len <= min_len)
+	{
+		now_print_len = repeat_print_fd(fd, " ", (int)(min_len - len));
+		if (now_print_len < 0)
+			return (-1);
+		len += now_print_len;
+	}
+	if (len > INT_MAX)
+		return (-1);
+	return (len);
+}
+
+int	right_aligned_print(int fd, const char *str, size_t min_len, int pad_zero)
+{
+	size_t	len;
+	char	*pad_str;
+	int		now_print_len;
+
+	len = ft_strlen(str);
+	if (len > INT_MAX || min_len > INT_MAX)
+		return (-1);
+	if (pad_zero != 0)
+		pad_str = "0";
+	else
+		pad_str = " ";
+	if (len <= min_len)
+	{
+		now_print_len = repeat_print_fd(fd, pad_str, (int)(min_len - len));
+		if (now_print_len < 0)
+			return (-1);
+		len += now_print_len;
+	}
+	if (len > INT_MAX)
+		return (-1);
+	if (write(fd, str, ft_strlen(str)) < 0)
+		return (-1);
+	return (len);
 }

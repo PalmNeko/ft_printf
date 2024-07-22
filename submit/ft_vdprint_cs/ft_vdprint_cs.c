408a6a652ed424c983cf19f5b569f329183b361c
tookuyam <tookuyam@42.student.fr>
2024-07-22 13:35:59 +0900

    update: for readable

--- submit/ft_vdprint_cs/ft_vdprint_cs.c
+++ submit/ft_vdprint_cs/ft_vdprint_cs.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vdprint_cs.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tookuyam <tookuyam@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:36:59 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/02/11 14:37:20 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/22 13:35:18 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,23 @@
 
 int	ft_vdprint_cs(int fd, t_cs *cs, va_list args)
 {
-	const t_relational_conversion_specifier_and_print_function	relation[] = {
-	{.specifier = CS_LOWER_S, .p_function = ft_vdprint_string_cs},
-	{.specifier = CS_LOWER_P, .p_function = ft_vdprint_pointer_cs},
-	{.specifier = CS_LOWER_C, .p_function = ft_vdprint_char_cs},
-	{.specifier = CS_LOWER_D, .p_function = ft_vdprint_decimal_cs},
-	{.specifier = CS_LOWER_U, .p_function = ft_vdprint_uint_cs},
-	{.specifier = CS_LOWER_I, .p_function = ft_vdprint_int_cs},
-	{.specifier = CS_LOWER_X, .p_function = ft_vdprint_hex_cs},
-	{.specifier = CS_UPPER_X, .p_function = ft_vdprint_hex_upper_cs},
-	{.specifier = CS_PERCENT, .p_function = ft_vdprint_percent_cs}};
-	int															relation_size;
-	int															index;
-
-	relation_size = sizeof(relation) / sizeof(relation[0]);
-	index = 0;
-	while (index < relation_size)
-	{
-		if (relation[index].specifier == cs->conversion_specifier)
-			return (relation[index].p_function(fd, cs, args));
-		index++;
-	}
+	if (cs->conversion_specifier == CS_LOWER_S)
+		return (ft_vdprint_string_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_LOWER_P)
+		return (ft_vdprint_pointer_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_LOWER_C)
+		return (ft_vdprint_char_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_LOWER_D)
+		return (ft_vdprint_decimal_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_LOWER_U)
+		return (ft_vdprint_uint_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_LOWER_I)
+		return (ft_vdprint_int_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_LOWER_X)
+		return (ft_vdprint_hex_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_UPPER_X)
+		return (ft_vdprint_hex_upper_cs(fd, cs, args));
+	else if (cs->conversion_specifier == CS_PERCENT)
+		return (ft_vdprint_percent_cs(fd, cs, args));
 	return (-1);
 }

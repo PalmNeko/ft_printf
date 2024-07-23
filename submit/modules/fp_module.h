47693fcf0345a85262d3e641702835fba315b916
tookuyam <tookuyam@42.student.fr>
2024-07-23 16:01:51 +0900

    add: fp_vdprint_cs function

--- submit/modules/fp_module.h
+++ submit/modules/fp_module.h
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 14:11:12 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/23 15:18:44 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/23 15:57:35 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 
 int		fp_print_aligned_left(int fd, int field_width, int *len, char *str);
 int		fp_print_aligned_right(int fd, int field_width, int *len, char *str);
+int		fp_vdprint_cs(int fd, t_cs *cs, va_list args);
 char	*fp_add_sign(int type, int *len, char *str);
 char	*fp_pad_for_num(int zero_field_width, int *size, char *str);
 char	*fp_int2str(int precision, int *len, int value);
 char	*fp_add_0x(bool be_add, int	*len, char *str);
 char	*fp_uint2str(int precision, int *len, unsigned int value, int base);
 char	*fp_cut_string(int length, int *size, char *str);
+char	*fp_to_upper(bool be_upper, int *len, char *str);
 char	*fp_uchr2str(int *size, unsigned char chr);
 
 #endif
---
+++ submit/modules/fp_vdprint_cs.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fp_vdprint_cs.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/23 15:20:24 by tookuyam          #+#    #+#             */
+/*   Updated: 2024/07/23 16:01:35 by tookuyam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+#include "conversion_specification.h"
+#include "fp_module.h"
+
+char	*fp_gen_str(t_cs *cs, int *len, char *str);
+char	*fp_gen_chrstr(t_cs *cs, int *len, unsigned int uchr);
+char	*fp_gen_uintstr(t_cs *cs, int *len, unsigned int value);
+char	*fp_gen_intstr(t_cs *cs, int *len, int value);
+
+int	fp_vdprint_cs(int fd, t_cs *cs, va_list args)
+{
+	char	*out_string;
+	int		len;
+
+	out_string = NULL;
+	if (cs->conversion_specifier == CS_LOWER_D
+		|| cs->conversion_specifier == CS_LOWER_I)
+		out_string = fp_gen_intstr(cs, &len, va_arg(args, int));
+	else if (cs->conversion_specifier == CS_LOWER_S)
+		out_string = fp_gen_str(cs, &len, va_arg(args, int));
+	else if (cs->conversion_specifier == CS_LOWER_C)
+		out_string = fp_gen_chrstr(cs, &len, va_arg(args, unsigned char));
+	else if (cs->conversion_specifier == CS_PERCENT)
+		return (write(fd, "%", 1));
+	else if (cs->conversion_specifier == CS_LOWER_X
+		|| cs->conversion_specifier == CS_UPPER_X
+		|| cs->conversion_specifier == CS_LOWER_U
+		|| cs->conversion_specifier == CS_LOWER_P)
+		out_string = fp_gen_uintstr(cs, &len, va_arg(args, unsigned int));
+	if (out_string == NULL)
+		return (-1);
+	if (cs->flag_minus)
+		return (fp_print_aligned_left(fd, cs->minimum_field_width, &len, out_string));
+	else
+		return (fp_print_aligned_right(fd, cs->minimum_field_width, &len, out_string));
+}
+
+char	*fp_gen_intstr(t_cs *cs, int *len, int value)
+{
+	char	*str;
+	int		sign_type;
+	int		zero_field_width;
+
+	sign_type = 0;
+	if (cs->flag_plus)
+		sign_type = 1;
+	else if (cs->flag_space)
+		sign_type = 2;
+	zero_field_width = 0;
+	if (cs->flag_zero && cs->is_specified_min_field_width)
+		zero_field_width = cs->minimum_field_width;
+	str = fp_add_sign(sign_type, len,
+			fp_pad_for_num(zero_field_width, len,
+				fp_int2str(cs->precision, len, value)));
+	return (str);
+}
+
+char	*fp_gen_uintstr(t_cs *cs, int *len, unsigned int value)
+{
+	char	*str;
+	int		zero_field_width;
+	int		base;
+
+	zero_field_width = 0;
+	if (cs->flag_zero && cs->is_specified_min_field_width)
+		zero_field_width = cs->minimum_field_width;
+	base = 10;
+	if (cs->conversion_specifier == CS_UPPER_X
+		|| cs->conversion_specifier == CS_LOWER_X)
+		base = 16;
+	str = fp_to_upper(cs->conversion_specifier == CS_UPPER_X, len,
+			fp_add_0x(cs->flag_sharp, len,
+				fp_pad_for_num(zero_field_width, len,
+					fp_uint2str(cs->precision, len, value, base))));
+	return (str);
+}
+
+char	*fp_gen_chrstr(t_cs *cs, int *len, unsigned int uchr)
+{
+	char	*str;
+
+	str = fp_uchr2str(len, uchr);
+	return (str);
+}
+
+char	*fp_gen_str(t_cs *cs, int *len, char *str)
+{
+	char	*str;
+	int		cut_size;
+
+	cut_size = INT_MAX;
+	if (cs->is_specified_precision == true)
+		cut_size = cs->precision;
+	str = fp_cut_string(cut_size, len, str);
+	return (str);
+}

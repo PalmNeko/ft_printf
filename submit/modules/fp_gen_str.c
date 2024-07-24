3f9c7951192fc35845a46297498912a8a0962757
tookuyam <tookuyam@42.student.fr>
2024-07-24 16:42:14 +0900

    fix: str

--- submit/modules/fp_gen_str.c
+++ submit/modules/fp_gen_str.c
@@ -6,21 +6,34 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 15:11:00 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/24 15:28:12 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/24 16:42:05 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "fp_module.h"
 #include "libft.h"
 
 char	*fp_gen_str(t_cs *cs, int *len, char *str)
 {
-	int		cut_size;
+	size_t	size;
 
-	cut_size = INT_MAX;
+	if (str == NULL)
+	{
+		if (cs->is_specified_precision && cs->precision < 6)
+			str = ft_strdup("");
+		else
+			str = ft_strdup("(null)");
+	}
+	else
+		str = ft_strdup(str);
 	if (cs->is_specified_precision == true)
-		cut_size = cs->precision;
-	str = fp_cut_string(cut_size, len, ft_strdup(str));
+		str = fp_cut_string(cs->precision, len, str);
+	size = ft_strnlen(str, (size_t)INT_MAX + 1);
+	if (size > INT_MAX)
+		return (free(str), NULL);
+	*len = size;
 	return (str);
 }

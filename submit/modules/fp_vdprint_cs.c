700eea7e3534fddf2a88bf8921c67388d4d888b0
tookuyam <tookuyam@42.student.fr>
2024-07-24 16:10:21 +0900

    fix: len ptr

--- submit/modules/fp_vdprint_cs.c
+++ submit/modules/fp_vdprint_cs.c
@@ -6,7 +6,7 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 15:20:24 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/24 15:28:02 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/24 16:09:53 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "libft.h"
 #include "fp_module.h"
 
-char	*fp_gen_vdprint_cs(t_cs *cs, va_list args);
+char	*fp_gen_vdprint_cs(t_cs *cs, int *len, va_list args);
 
 int	fp_vdprint_cs(int fd, t_cs *cs, va_list args)
 {
@@ -26,7 +26,7 @@ int	fp_vdprint_cs(int fd, t_cs *cs, va_list args)
 	out_string = NULL;
 	if (cs->conversion_specifier == CS_PERCENT)
 		return (write(fd, "%", 1));
-	out_string = fp_gen_vdprint_cs(cs, args);
+	out_string = fp_gen_vdprint_cs(cs, &len, args);
 	if (out_string == NULL)
 		return (-1);
 	if (cs->flag_minus)
@@ -35,25 +35,23 @@ int	fp_vdprint_cs(int fd, t_cs *cs, va_list args)
 		return (fp_print_aligned_right(fd, cs->minimum_field_width, &len, out_string));
 }
 
-char	*fp_gen_vdprint_cs(t_cs *cs, va_list args)
+char	*fp_gen_vdprint_cs(t_cs *cs, int *len, va_list args)
 {
 	char	*out_string;
-	int		len;
 
-	len = 0;
 	out_string = NULL;
 	if (cs->conversion_specifier == CS_LOWER_D
 		|| cs->conversion_specifier == CS_LOWER_I)
-		out_string = fp_gen_intstr(cs, &len, va_arg(args, int));
+		out_string = fp_gen_intstr(cs, len, va_arg(args, int));
 	else if (cs->conversion_specifier == CS_LOWER_S)
-		out_string = fp_gen_str(cs, &len, va_arg(args, char *));
+		out_string = fp_gen_str(cs, len, va_arg(args, char *));
 	else if (cs->conversion_specifier == CS_LOWER_C)
-		out_string = fp_gen_chrstr(cs, &len, (unsigned char)va_arg(args, int));
+		out_string = fp_gen_chrstr(cs, len, (unsigned char)va_arg(args, int));
 	else if (cs->conversion_specifier == CS_LOWER_X
 		|| cs->conversion_specifier == CS_UPPER_X
 		|| cs->conversion_specifier == CS_LOWER_U)
-		out_string = fp_gen_uintstr(cs, &len, va_arg(args, unsigned int));
+		out_string = fp_gen_uintstr(cs, len, va_arg(args, unsigned int));
 	else if (cs->conversion_specifier == CS_LOWER_P)
-		out_string = fp_gen_ptrstr(cs, &len, va_arg(args, unsigned int));
+		out_string = fp_gen_ptrstr(cs, len, va_arg(args, unsigned int));
 	return (out_string);
 }

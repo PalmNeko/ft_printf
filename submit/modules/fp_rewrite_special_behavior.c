36c20eedd48c1922a5ff499d999b463f8070ce2d
tookuyam <tookuyam@42.student.fr>
2024-07-25 16:37:46 +0900

    update: for prototype delare

--- submit/modules/fp_rewrite_special_behavior.c
+++ submit/modules/fp_rewrite_special_behavior.c
@@ -6,12 +6,32 @@
 /*   By: tookuyam <tookuyam@student.42tokyo.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 14:24:47 by tookuyam          #+#    #+#             */
-/*   Updated: 2024/07/24 16:12:18 by tookuyam         ###   ########.fr       */
+/*   Updated: 2024/07/25 16:37:10 by tookuyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fp_module.h"
 #include <stdio.h>
+
+void	fp_rewrite_precision(t_cs *cs);
+void	fp_rewrite_flag_zero(t_cs *cs);
+
+void	fp_rewrite_special_behavior(t_cs *cs)
+{
+	if (cs->flag_minus == true)
+		cs->flag_zero = false;
+	if (cs->flag_plus == true)
+		cs->flag_space = false;
+	fp_rewrite_flag_zero(cs);
+	fp_rewrite_precision(cs);
+	if (cs->conversion_specifier != CS_LOWER_X
+		&& cs->conversion_specifier != CS_UPPER_X
+		&& cs->conversion_specifier != CS_LOWER_P)
+		cs->flag_sharp = false;
+	if (cs->conversion_specifier == CS_LOWER_P)
+		cs->flag_sharp = true;
+	return ;
+}
 
 void	fp_rewrite_precision(t_cs *cs)
 {
@@ -35,21 +55,4 @@ void	fp_rewrite_flag_zero(t_cs *cs)
 			|| cs->conversion_specifier == CS_LOWER_P
 			|| cs->conversion_specifier == CS_UPPER_X))
 		cs->flag_zero = false;
-}
-
-void	fp_rewrite_special_behavior(t_cs *cs)
-{
-	if (cs->flag_minus == true)
-		cs->flag_zero = false;
-	if (cs->flag_plus == true)
-		cs->flag_space = false;
-	fp_rewrite_flag_zero(cs);
-	fp_rewrite_precision(cs);
-	if (cs->conversion_specifier != CS_LOWER_X
-		&& cs->conversion_specifier != CS_UPPER_X
-		&& cs->conversion_specifier != CS_LOWER_P)
-		cs->flag_sharp = false;
-	if (cs->conversion_specifier == CS_LOWER_P)
-		cs->flag_sharp = true;
-	return ;
 }
